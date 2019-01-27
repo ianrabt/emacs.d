1358a11e8b66b24a5b3ebb8ddef00419cd08f2ef
Ian Taylor <itaylor@hmc.edu>
2019-01-27 15:04:19 -0800

    make evil more friendly with olivetti mode
    And work better with long-line prose in general

--- lisp/general.el
+++ lisp/general.el
@@ -2,10 +2,29 @@
 ;;   this file is a bit of a grab-bag
 ;; =====================================
 
+;; evil
+;; ====
+
 ;; start up evil automatically
 (use-package evil
   :config
   (evil-mode 1))
+
+;; Make movement keys work like they should
+(define-key evil-normal-state-map (kbd "<remap> <evil-next-line>")
+  'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>")
+  'evil-previous-visual-line)
+(define-key evil-motion-state-map (kbd "<remap> <evil-next-line>")
+  'evil-next-visual-line)
+(define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>")
+  'evil-previous-visual-line)
+
+; Make horizontal movement cross lines                                    
+(setq-default evil-cross-lines t)
+
+;; misc.
+;; =====
 
 ;; ivy config
 (use-package ivy
