8cff603679f5c4d94d5a694f8bfeb8eb9cffe47b
Ian Taylor <itaylor@hmc.edu>
2019-05-17 17:04:05 -0700

    fix emacsclient theme

--- lisp/graphical.el
+++ lisp/graphical.el
@@ -7,6 +7,14 @@
 (setq nord-region-highlight "frost")
 (load-theme 'nord t)
 
+(defun load-nord-theme (frame)
+  (select-frame frame)
+  (load-theme 'nord t))
+
+(if (daemonp)
+	(add-hook 'after-make-frame-functions #'load-nord-theme)
+  (load-theme 'nord t))
+
 ;; toggles
 ;; =======
 (tool-bar-mode -1)
