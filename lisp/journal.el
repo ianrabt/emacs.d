45a87044814c02570c98d21514959da5ad5def14
Ian Taylor <itaylor@hmc.edu>
2019-10-17 22:24:49 -0700

    add ability to encrypt journal entries
    This will require a lot more work to make robust, however.

--- lisp/journal.el
+++ lisp/journal.el
@@ -22,5 +22,37 @@
 
 (defun journal-insert-entry ()
   (interactive)
-  (org-insert-heading-after-current)
-  (org-time-stamp-inactive))
+  (end-of-buffer)
+  (org-insert-heading-respect-content)
+  (let ((current-prefix-arg '(16)))
+    (call-interactively 'org-time-stamp-inactive))
+  (newline-and-indent)
+  (evil-append-line 1))
+
+(defvar journal-file-directory
+  "~/Insync/itaylor@g.hmc.edu/Google Drive/Personal/journal/")
+
+(defvar journal-file-format-string "%Y %b.org.gpg"
+  "The time format string, used to create and find the
+appropriate journal file in the journal directory.
+
+This string indicates how often new files will be created, the
+mode used for the journal files (see file extension) and whether
+the file is encrypted (append \".gpg\" to the filename)
+
+It is passed as an argument to `parse-time-string' and
+`format-time-string'")
+
+(defun journal-find-file (&optional time zone)
+  "TODO"
+  (interactive)
+  ;; TODO use optional arguments!
+  (let ((filename (format-time-string journal-file-format-string)))
+    (find-file (concat journal-file-directory filename)))
+  (journal-mode))
+
+(defun journal-new-entry ()
+  "TODO"
+  (interactive)
+  (journal-find-file)
+  (journal-insert-entry))
